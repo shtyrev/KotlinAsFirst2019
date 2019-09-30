ed7d27263f745923cfb807b775307d356aa81339
Alexander <sasha00hk00@gmail.com>
2019-09-30 22:16:43 +0300

    lesson2.task1 ageDescription: Complete timeForHalfWay: Complete whichRookThreatens: Complete rookOrBishopThreatens: Complete

--- test/lesson2/task1/Tests.kt
+++ test/lesson2/task1/Tests.kt
@@ -34,7 +34,6 @@ class Tests {
     @Test
     @Tag("Easy")
     fun ageDescription() {
-        assertEquals("2 года", ageDescription(2))
         assertEquals("1 год", ageDescription(1))
         assertEquals("21 год", ageDescription(21))
         assertEquals("132 года", ageDescription(132))
