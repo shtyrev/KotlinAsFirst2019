0e040829717c7aa63a2bd54b0877a70cd895f9b8
Alexander <sasha00hk00@gmail.com>
2019-10-16 23:24:36 +0300

    lesson2 task1 ageDescription: fixed triangleKind: fixed
    task2
    isNumberHappy: fixed
    queenThreatens: fixed
    daysInMonth: fixed
    brickPasses: fixed
    
    lesson3.task1
    digitNumber: complete
    fib: complete
    lcm: complete
    minDivisor: complete
    maxDivisor: complete

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -148,7 +148,9 @@ fun maxDivisor(n: Int): Int {
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean = TODO()/*{
+
+}*/
 
 /**
  * Простая
